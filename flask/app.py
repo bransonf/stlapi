6306bcce9c264c94024e0e316c224cfe09c53a37
bransonf <32143148+bransonf@users.noreply.github.com>
2019-12-12 15:05:59 -0600

    implement first crime query endpoint

--- flask/app.py
+++ flask/app.py
@@ -1,3 +1,4 @@
+#!./env/bin/python3
 # Flask API for Serving STL Data
 from flask import Flask, render_template, jsonify, request
 from flask_api import status
@@ -5,6 +6,22 @@ from flask_cors import CORS
 
 from os import environ
 
+import yaml
+import psycopg2
+
+# load creds
+with open(r'../creds.yml') as file:
+    creds = yaml.safe_load(file)
+
+# connect to database
+dbcon = psycopg2.connect(host = creds['host'],
+                        database = creds['database'],
+                        user = creds['username'],
+                        password = creds['password'])
+
+from crime_queries import coords
+
+# initialize flask
 app = Flask(__name__)
 CORS(app) # Add Cors Exception
 
@@ -12,6 +29,24 @@ CORS(app) # Add Cors Exception
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Define Crime Endpoints
+@app.route('/crime/coords')
+def crimecoords():
+    # Get URL Params
+    start = request.args.get('start',None)
+    end = request.args.get('end',None)
+    category = request.args.get('category', 'all')
+    proj = request.args.get('proj','wgs')
+    unfounded = request.args.get('unfounded',False)
+
+    rsp = coords(dbcon, start, end, category, proj, unfounded)
+
+    return jsonify(rsp)
+
+
+# import the crime endpoint functions
+# sanitize url input
 
 # Run the App
 if __name__ == "__main__":
---
+++ flask/crime_queries.py
@@ -0,0 +1,67 @@
+# Crime Queries
+
+import yaml
+import psycopg2
+
+# load creds
+with open(r'../creds.yml') as file:
+    creds = yaml.safe_load(file)
+
+# connect to database
+dbcon = psycopg2.connect(host = creds['host'],
+                        database = creds['database'],
+                        user = creds['username'],
+                        password = creds['password'])
+
+
+# define functions for interacting with database (Crime table)
+
+# Get Coordinates of Crimes
+def coords(con, start, end=None, category='all', proj='wgs', unfounded=False):
+    cur = con.cursor()
+
+    # Build and Execute Query
+    if proj == 'wgs':
+        qry = 'SELECT "wgsX", "wgsY", "crimeCat" FROM crime'
+    elif proj == 'nad':
+        qry = 'SELECT "nadX", "nadY", "crimeCat" FROM crime'
+
+    # Unfounded
+    if not unfounded:
+        qry = qry + ' WHERE "count" = true' 
+    else:
+        qry = qry + ' WHERE "count" = false'
+
+    # Category Handling
+    if category != 'all':
+        qry = qry + ' AND "crimeCat" = ' + '\'' + category + '\''
+
+    # Date Range
+    if end == None:
+        end = start
+    
+    qry = qry + ' AND "dateOccur" BETWEEN date \'' + start + '\' AND date \'' + end + '\';' 
+
+    cur.execute(qry)
+
+    q = cur.fetchall()
+    cur.close()
+    return(list(q))
+
+# Get Details Based on ID of Crime
+def detail(con, id=None):
+    cur = con.cursor()
+    cur.execute()
+
+    q = cur.fetchall()
+    cur.close()
+    return(list(q))
+
+# Get Summary Based on Location/Date
+def summary(con, start, end, category, unfounded=False, neighborhood=None, district=None):
+    cur = con.cursor()
+    cur.execute()
+
+    q = cur.fetchall()
+    cur.close()
+    return(list(q))
